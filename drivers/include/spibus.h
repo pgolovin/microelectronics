2684c911132680a048f3eb909aa9c002d3b94395
pgolovin <pavel.golovinskiy@gmail.com>
2022-01-07 21:33:31 +0300

    SPI bus basic functionality. Add remove and select devices

--- drivers/include/spibus.h
+++ drivers/include/spibus.h
@@ -14,12 +14,22 @@ struct SPI_BUS_type
 };
 typedef struct SPI_BUS_type * HSPIBUS;
 
+typedef enum SPIBUS_Status_type
+{
+    SPIBUS_OK,
+    SPIBUS_FAIL = 0xFF
+} SPIBUS_Status;
+
+#define SPIBUS_DeviceLimit 10
+
 HSPIBUS SPIBUS_Configure(SPI_HandleTypeDef* hspi, uint32_t timeout);
-uint8_t SPIBUS_AddPeripherialDevice(HSPIBUS hspi, GPIO_TypeDef* cs_port_array, uint16_t sc_port);
+void SPIBUS_Release(HSPIBUS hspi);
+
+SPIBUS_Status SPIBUS_AddPeripherialDevice(HSPIBUS hspi, GPIO_TypeDef* cs_port_array, uint16_t sc_port);
 
 // selected device means all low
-void SPIBUS_SelectDevice(HSPIBUS hspi, uint32_t id);
-void SPIBUS_UnselectDevice(HSPIBUS hspi, uint32_t id);
+SPIBUS_Status SPIBUS_SelectDevice(HSPIBUS hspi, uint32_t id);
+SPIBUS_Status SPIBUS_UnselectDevice(HSPIBUS hspi, uint32_t id);
 void SPIBUS_UnselectAll(HSPIBUS hspi);
 
 // fasade on HAL_SPI functions to hide internal implementation of SPIBUS 'class'
